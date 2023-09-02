4f98c631d4aacf617bbfc6f06355ab012c0ea69b
SMLukwiya <sundaymorganl@gmail.com>
2023-09-02 19:28:07 +0300

    fix lint

--- app/api/errorHandler/route.ts
+++ app/api/errorHandler/route.ts
@@ -51,7 +51,8 @@ const errorTypes = {
   fvr: 'Failed Verification Email Request'
 };
 
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEB_HOOK || '';
+const DISCORD_WEBHOOK_URL =
+  'https://discord.com/api/webhooks/1147222511201824898/PRLd3CpMNqDnJ8xzbq3No6Pbnl2epL5GPA1yQqp76S9WvOECtBA7vZH99EJ9PEqIRVBk';
 
 async function sendToDiscord(message: string) {
   const payload = {
